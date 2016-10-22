eb18690456b507da30eaa75e3c7e09e7c978c9bb
Alexander Harchenko <morontt@gmail.com>
2016-10-22 11:18:07 +0300

    simple grunt config :sweat_smile:

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            css: {
+                files: "styles/src/*.less",
+                tasks: ["less", "concat:css", "cssmin"]
+            },
+            js: {
+                files: "src/*.js",
+                tasks: ["concat:js", "uglify"]
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["styles/src/"]
+                },
+                files: {
+                    "styles/css/app.css": "styles/src/app.less"
+                }
+            }
+        },
+        concat: {
+            css: {
+                options: {
+                    stripBanners: {
+                        block: true
+                    },
+                    banner: '/*! <%= pkg.name %> --- <%= grunt.template.today("dd mmm yyyy HH:MM:ss") %> */\n'
+                },
+                src: ['node_modules/normalize.css/normalize.css', 'styles/css/app.css'],
+                dest: 'assets/<%= pkg.name %>.css'
+            },
+            js: {
+                src: ['src/app.js'],
+                dest: 'assets/<%= pkg.name %>.js'
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'assets/<%= pkg.name %>.min.css': 'assets/<%= pkg.name %>.css'
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> ' +
+                '--- <%= grunt.template.today("dd mmm yyyy HH:MM:ss") %> */\n'
+            },
+            dist: {
+                files: {
+                    'assets/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+                }
+            }
+        },
+        'http-server': {
+            'dev': {
+                root: '',
+                port: 8282,
+                host: '127.0.0.1',
+                autoIndex: true,
+                ext: 'html',
+                runInBackground: true,
+                openBrowser: false
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['http-server', 'watch']);
+};
